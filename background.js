9ba5e4137eb9631779c0a8d145e0cf5aaa140387
RNZZL <46511591+RNZZL@users.noreply.github.com>
2019-02-26 07:48:37 +0800

    support 10 web page

--- background.js
+++ background.js
@@ -1,4 +1,9 @@
 console.log("background running");
+let searchFlag;
+let searchQuery;
+let searchTime;//by ZZL
+
+
 chrome.runtime.onMessage.addListener(handleMessage);
 chrome.runtime.onInstalled.addListener(function (details) {
     chrome.storage.local.clear(function () {
@@ -22,26 +27,49 @@ function update(array)//array = behaviorItem in content.js
 }
 
 function handleMessage(request, sender, sendResponse) {
-    let behaviorCodes = ["copy", "select", "search"];
-    if (behaviorCodes.includes(request.eventtype)) {
-        chrome.storage.local.get(
-            ['behaviorItems'],// get the data whose key =behaviorItems
-            function (result) {
-                let bhvItems = result.behaviorItems;
-                if (!Array.isArray(bhvItems)) {
-                    update([]);//without this push is undefined?
-                } else {
-                    console.log("request: ", request);
-                    bhvItems.push(request);
-                    update(bhvItems);
-                    console.log(bhvItems);
-                }
-            });
-    }
+    if (request.eventtype === 'search') {
+        searchFlag = true;
+        console.log(request.data);
+        searchQuery = request.data;
+        searchTime = request.time;
+    }//byZZL
     return true;
 }
 
 
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+
+    if (changeInfo.status === "complete") {
+        console.log("new url detected:" + tab.url);
+        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {url: tab.url}, function (response) {
+            });
+        });
+    }
+    if (searchFlag === true) {//by ZZL
+        chrome.storage.local.get(
+            ['behaviorItems'],
+            function (result) {
+                console.log(result);
+                let searchItems = result.behaviorItems;
+                if (!Array.isArray(searchItems)) {
+                    update([]);
+                } else {
+                    let srchItem = {
+                        "eventtype": "search",
+                        "time": searchTime,
+                        "url": tab.url,
+                        "title": tab.title,
+                        "data": searchQuery,
+                    };
+                    searchItems.push(srchItem);
+                    update(searchItems);
+                }
+            }
+        );
+    }
+    searchFlag = false;//by ZZL
+});
 
 
 
--- scripts/contentscript/content.js
+++ scripts/contentscript/content.js
@@ -1,17 +1,35 @@
-function newUrlHandler()
-{
-    let url = document.location.href;
+function newUrlHandler() {
+    let currentUrl = document.location.href;
     let query;
-    if (url.includes("google.com")) {
-        const regex = /(?<=q=).*?(?=&)/s;
-        if (url.match(regex) !== null) {
-            query = url.match(regex)[0].replace(/\+/g, ' ');
+    //other approach:
+    if (currentUrl.includes(".amazon.com")) {
+        const regex = /(?<=k=).*?(?=&)/s;//".amazon.com"
+        //const regex = /(?<=find_desc=).*?(?=&)/s;//".yelp.com"
+        //const regex = /(?<=keywords=).*?(?=&)/s;//".linkedin.com"
+
+        if (currentUrl.match(regex) !== null) {
+            query = currentUrl.match(regex)[0].replace(/\+/g, ' ');
             let behaviorItem = makeBehaviorItem("search", query);
             chrome.runtime.sendMessage(behaviorItem, (response) => {
                 console.log("Message Response: ", response); //Response is undefined.
             });
-        }
+        }//for amazon yelp and linkedin
     }
+    //const regex = /search_query=(.*)/;//".youtube.com"?
+    //const regex = /q=(.*)/;//".github.com"?
+    //const regex = /q=(.*)/;//".stackoverflow.com"?
+    //const regex = /wiki/(.*)/;//".wikipedia.org"?
+    //const regex = /q=(.*)/;//".bbc.co.uk"?//regex=/(?<=q=).*?(?=&)/s
+    //const regex = /q=(.*)/;//".ted.com"?
+    //const regex = /#search/(.*)/;//".mail.google.com"?
+    if (currentUrl.includes(".github.com")) {
+        const regex = /q=(.*)/;//".github.com"?
+        query = currentUrl.match(regex)[1];
+        let behaviorItem = makeBehaviorItem("search", query);
+        chrome.runtime.sendMessage(behaviorItem, (response) => {
+            console.log("Message Response: ", response); //Response is undefined.
+        });
+    }//for left
     //TODO: collect url history here.
 }
 
@@ -28,3 +46,10 @@ function makeBehaviorItem(event_type, content)
 
 window.addEventListener("load", newUrlHandler);
 
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    newUrlHandler();
+});
+
+/*var str = 'http://zhipur.com/item?data=SN120180525FEOCE';
+var code2 = str.match(/data=(.*)/)[1];
+console.log(code2);*///test zzl
